e437f36115e25b7b158ec3d64cfd1d771dd6d9e1
ngonzo644 <nichoja20@gmail.com>
2023-11-16 19:32:58 -0500

    commit

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 from alien import Alien
+from player import Player
 import pygame
 
 class Controller:
@@ -6,9 +7,11 @@ class Controller:
         pygame.init()
         self.display=pygame.display.set_mode()
         self.width,self.height=pygame.display.get_window_size()
-        # self.space=pygame.surface((self.width,self.height))
     def mainloop(self):
         background=pygame.image.load("assets/Space001.png")
+        player1=Player(500,500,"assets/image.png")
+        player1_group=pygame.sprite.Group()
+        player1_group.add(player1)
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -16,5 +19,5 @@ class Controller:
                     break
         
             self.display.blit(background,(0,0))
-            
+            player1_group.draw(self.display)
             pygame.display.flip()        
