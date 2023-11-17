2e1668610675fd9ba646efb2053f0693f898f042
Amadou <barriamadou05@icloud.com>
2023-11-17 12:52:50 -0500

    commit

--- src/controller.py
+++ src/controller.py
@@ -10,7 +10,8 @@ class Controller:
         self.display=pygame.display.set_mode()
         self.width,self.height=pygame.display.get_window_size()
         self.background=pygame.image.load("assets/Space001.png")
-
+        self.clock = pygame.time.Clock()
+        self.clock.tick(60)
         
     def mainloop(self):
         self.bg_rect = self.background.get_rect(topleft = (0,0))
@@ -20,7 +21,7 @@ class Controller:
         laser_group = pygame.sprite.Group()
         laser = Laser(500, 500)
         laser_group.add(laser)
-        alien1=Alien(random.randint(0,1000),200,"assets/spaceship.png")
+        alien1=Alien(random.randint(0,1400),200,"assets/spaceship.png")
         alien_group=pygame.sprite.Group()
         alien_group.add(alien1)
         
@@ -35,7 +36,7 @@ class Controller:
                         laser.rect.center=player1.rect.center
                         while laser.rect.y>0:
                             if laser.rect.colliderect(alien1.rect):
-                                alien_group.remove(alien1)
+                                alien_group.remove(alien1)                             
                             y=laser.rect.y-8
                             laser_group.update(y)
                             laser_group.draw(self.display)
@@ -50,7 +51,10 @@ class Controller:
 
             
             player1_group.draw(self.display)
-            for i in range (0,6):
+            for i in range(0,6):
+                 alien_group.add(alien1)
+                 alien_group.draw(self.display)
 
-                alien_group.draw(self.display)
+                 
+
             pygame.display.flip()        
