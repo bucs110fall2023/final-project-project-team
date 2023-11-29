be5d7ef21ac2e59f75df42afb1676028abaea4e9
ngonzo644 <nichoja20@gmail.com>
2023-11-29 18:36:12 -0500

    laser stops once it hits one alien

--- src/controller.py
+++ src/controller.py
@@ -22,7 +22,7 @@ class Controller:
         laser = Laser(500, 500)
         laser_group.add(laser)
         # alien1=Alien(random.randint(0,1400),200,"assets/spaceship.png")
-        num_aliens = 10
+        num_aliens = 6
         alien_group=pygame.sprite.Group()
         for alien in range(num_aliens):
             alien = Alien(random.randint(0,1400), random.randint(0,300), "assets/spaceship.png")
@@ -31,6 +31,7 @@ class Controller:
         # level design?
         speed=1
         level=1
+
 
             
         
@@ -45,10 +46,10 @@ class Controller:
                         laser.beam()
                         laser.rect.center=player1.rect.center
                         while laser.rect.y>0:
-                            # if pygame.sprite.groupcollide(laser_group, alien_group, False, True):
-                                # alien_group.remove(alien)     
-                            pygame.sprite.spritecollide(laser,alien_group,True)                   
-                            y=laser.rect.y-8
+                            y=laser.rect.y-1
+                            hit = pygame.sprite.spritecollide(laser,alien_group,True)   
+                            if hit:
+                                break                
                             laser_group.update(y)
                             laser_group.draw(self.display)
             pressed=pygame.key.get_pressed()
@@ -60,17 +61,18 @@ class Controller:
                 player1_group.update(x)
             
             if not alien_group:
-                level+=1
+                level+=1                    
                 if level == 2:
-                  num_aliens = 10
+                  num_aliens = 8
                 if level == 3:
-                    speed=1
+                    speed=1.2
+                    num_aliens = 11
+                if level == 4:
                     num_aliens = 15
-                    
                 for alien in range(num_aliens):
                     alien = Alien(random.randint(0,1400), random.randint(0,500), "assets/spaceship.png")
                     alien_group.add(alien)
-                
+
             for a in range(num_aliens):
                 alien_group.update(self.width,speed)
                 alien_group.draw(self.display)
