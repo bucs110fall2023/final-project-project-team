af9ec19fa35163c684c70b9836099ed69b676e0b
Amadou <barriamadou05@icloud.com>
2023-12-06 17:02:16 -0500

    docstrings

--- src/player.py
+++ src/player.py
@@ -2,6 +2,12 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, img):
+        """
+        This class contains the parameters for the player character including the image
+        and rectangle we use for making up the player sprite.
+        Args: x = x position, y = y position, img = ship image
+        
+        """
         super().__init__()
         self.image=pygame.image.load(img)
         self.image = pygame.transform.scale(self.image,(100,100))
